2afc73c0fe344d8c7c1f4b9be6488466124525cf
DL <444c43@gmail.com>
2017-09-07 16:10:05 -0400

    add attorney table inserts

--- convert_xml.rb
+++ convert_xml.rb
@@ -5,12 +5,12 @@ require 'active_support/core_ext/hash'
 require 'nokogiri'
 require 'byebug'
 
-
 class XMLParser
   def initialize(filename)
     @filename = filename
     @file = File.read("files/#{filename}.xml")
     @doc = Hash.from_xml(@file)
+    @party_attorney_headers = []
     @party_headers = []
     @case_headers = case_headers
     @conn = PG.connect( dbname: 'probate' )
@@ -32,25 +32,41 @@ class XMLParser
       end.compact.join(',')
 
       sql = "INSERT INTO cases (#{columns}) VALUES (#{data})"
-      #puts "case: " + sql
       @conn.exec(sql)
     end
   end
 
   def update_party_table(items)
-    items.each { |item| real_update(item) } if items.is_a?(Array)
-    real_update(items) if items.is_a?(Hash)
+    items.each { |item| update_party(item) } if items.is_a?(Array)
+    update_party(items) if items.is_a?(Hash)
   end
 
-  def real_update(enum_value)
+  def update_party(enum_value)
+    columns = enum_value.map { |key, value| key if value.is_a?(String) }.compact.join(",")
+
+    data = enum_value.map do |key, value|
+      update_party_attorney_table(value) if key == "PartyAttorney" && is_a_collection?(value)
+      %Q('#{value.gsub("'", "''")}') if value.is_a?(String)
+    end.compact.join(',')
+
+    sql = "INSERT INTO party (#{columns}) VALUES (#{data})"
+    @conn.exec(sql)
+  end
+
+  def update_party_attorney_table(items)
+    items.each { |item| update_party_attorney(item) } if items.is_a?(Array)
+    update_party_attorney(items) if items.is_a?(Hash)
+  end
+
+  def update_party_attorney(enum_value)
     columns = enum_value.map { |key, value| key if value.is_a?(String) }.compact.join(",")
 
     data = enum_value.map do |key, value|
       %Q('#{value.gsub("'", "''")}') if value.is_a?(String)
     end.compact.join(',')
+    return if columns == '' or data == ''
 
-    sql = "INSERT INTO party (#{columns}) VALUES (#{data})"
-    #puts "party: " + sql
+    sql = "INSERT INTO attorney (#{columns}) VALUES (#{data})"
     @conn.exec(sql)
   end
 
@@ -78,9 +94,19 @@ class XMLParser
 
     party_names = []
 
-    collection.each { |key, value| party_names << key if is_string_or_nil?(value) } if collection.is_a?(Hash)
+    collection.each { |key, value| is_string_or_nil?(value) ? party_names << key : party_attorney_headers(value) } if collection.is_a?(Hash)
 
     party_names.compact.each { |name| @party_headers << name unless @party_headers.include? name }
+  end
+
+  def party_attorney_headers(collection)
+    collection.each { |element| party_attorney_headers(element) } if collection.is_a?(Array)
+
+    headers = []
+
+    collection.each { |key, value| headers << key if is_string_or_nil?(value) } if collection.is_a?(Hash)
+
+    headers.compact.each { |name| @party_attorney_headers << name unless @party_attorney_headers.include? name }
   end
 
   private
